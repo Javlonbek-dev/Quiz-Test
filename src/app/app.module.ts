b99e535755087837410b48c6df771e874320ba40
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-06-27 12:50:41 +0300

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionComponent } from './question/question.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ChangeBgDirective } from './change-bg.directive';
+import { GeneralComponent } from './general/general.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { ChangeBgDirective } from './change-bg.directive';
     WelcomeComponent,
     QuestionComponent,
     HeaderComponent,
-    ChangeBgDirective
+    ChangeBgDirective,
+    GeneralComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,6 +6,16 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
+name: string = '';
+
+  onSubmit(name:string) {
+    if (this.name.trim() === '') {
+      alert("Ism kirit!!!")
+     
+    } else {
+      console.log('Name:', this.name);
+    }
+  }
 
   @ViewChild('name') nameKey!:ElementRef;
 
